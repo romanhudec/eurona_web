9f048acfa898453dc01e4e51499d286a51c09fc2
roman.hudec@rhudec.sk <roman.hudec@zentity.com>
2018-09-03 11:38:41 +0200

    Uprava reportov

--- portal/eurona/user/advisor/reports/noviPoradci.aspx.cs
+++ portal/eurona/user/advisor/reports/noviPoradci.aspx.cs
@@ -6,17 +6,46 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Data;
 using System.Data.SqlClient;
+using UzavierkaEntity = Eurona.Common.DAL.Entities.Uzavierka;
 using Telerik.Web.UI;
 
 namespace Eurona.User.Advisor.Reports {
     public partial class NoviPoradciReport : ReportPage {
+        private DateTime uzavierkaFrom;
+        private DateTime uzavierkaTo;
+        private int? obdobi;
         protected void Page_Load(object sender, EventArgs e) {
             if (this.ForAdvisor == null) return;
             if (this.ForAdvisor.TVD_Id == null) return;
 
+            #region Uzavierka
+            UzavierkaEntity uzavierkaBefore = Storage<UzavierkaEntity>.ReadFirst(new UzavierkaEntity.ReadById { UzavierkaId = (int)UzavierkaEntity.UzavierkaId.EuronaBefor });
+            if (uzavierkaBefore == null) {
+                uzavierkaBefore = new UzavierkaEntity();
+                DateTime beforeOd = GetLastDayOfMonth(DateTime.Now.Year, DateTime.Now.AddMonths(-1).Month);
+                uzavierkaFrom = new DateTime(beforeOd.Year, beforeOd.Month, beforeOd.Day, 0, 0, 0);
+                uzavierkaTo = new DateTime(beforeOd.Year, beforeOd.Month, beforeOd.Day, 23, 59, 59);
+            } else {
+                uzavierkaFrom = uzavierkaBefore.UzavierkaDo.Value;
+            }
+            UzavierkaEntity uzavierka = Storage<UzavierkaEntity>.ReadFirst(new UzavierkaEntity.ReadById { UzavierkaId = (int)UzavierkaEntity.UzavierkaId.Eurona });
+            if (uzavierka == null) {
+                uzavierka = new UzavierkaEntity();
+                DateTime datumDo = GetLastDayOfMonth(DateTime.Now.Year, DateTime.Now.Month);
+                uzavierkaFrom = new DateTime(datumDo.Year, datumDo.Month, datumDo.Day, 0, 0, 0);
+                uzavierkaTo = new DateTime(datumDo.Year, datumDo.Month, datumDo.Day, 23, 59, 59);
+            } else {
+                uzavierkaTo = uzavierka.UzavierkaDo.Value;
+            }
+            #endregion
+
             string code = this.ForAdvisor.Code;
             GridViewDataBind(!IsPostBack);
         }
+        private DateTime GetLastDayOfMonth(int year, int month) {
+            return new DateTime(year, month, DateTime.DaysInMonth(year, month));
+        }
+
         public override RadGrid GetGridView() {
             return this.gridView;
         }
@@ -29,7 +58,7 @@ namespace Eurona.User.Advisor.Reports {
         }
 
         private void GridViewDataBind(bool bind) {
-            int? obdobi = null;
+            this.obdobi = null;
             object filter = GetFilter();
             if (filter != null) obdobi = (int)filter;
             if (!obdobi.HasValue || obdobi.ToString().Length != 6) return;
@@ -43,6 +72,28 @@ namespace Eurona.User.Advisor.Reports {
 
             int obdobi1 = Convert.ToInt32(obdobi.ToString().Substring(0, 4)) * 100 + mesic1;
             int obdobi2 = Convert.ToInt32(obdobi.ToString().Substring(0, 4)) * 100 + mesic2;
+
+            #region Uzavierka
+            int currentObdobiRRRRMM = this.CurrentObdobiRRRRMM;
+            if (uzavierkaTo <= DateTime.Now) {
+                int year = uzavierkaTo.Year * 100;
+                currentObdobiRRRRMM = year + uzavierkaTo.Month;
+                if (this.CurrentObdobiRRRRMM == currentObdobiRRRRMM) {
+                    DateTime date = uzavierkaTo.AddMonths(-1);
+                    year = date.Year * 100;
+                    currentObdobiRRRRMM = year + date.Month;
+                }
+            } else if (uzavierkaFrom <= DateTime.Now && uzavierkaTo >= DateTime.Now && uzavierkaFrom.Month != uzavierkaTo.Month) {
+                DateTime date = uzavierkaTo.AddMonths(-1);
+                int year = date.Year * 100;
+                currentObdobiRRRRMM = year + date.Month;
+            }
+
+            #endregion
+
+            if (this.obdobi == 0) {
+                this.obdobi = currentObdobiRRRRMM;
+            }
 
             CMS.Pump.MSSQLStorage tvdStorage = new CMS.Pump.MSSQLStorage(base.ConnectionString);
             using (SqlConnection connection = tvdStorage.Connect()) {
