56a9cd8cfadf9c3473b8df4973f07e1744f5fc65
roman.hudec@rhudec.sk <roman.hudec@zentity.com>
2019-09-05 09:09:37 +0200

    email verfication remoce cancel button

--- portal/eurona/user/RequestEmailVerifyControl.ascx.designer.cs
+++ portal/eurona/user/RequestEmailVerifyControl.ascx.designer.cs
@@ -58,15 +58,6 @@ namespace Eurona.User {
         protected global::System.Web.UI.WebControls.Label lblValidatorText;
         
         /// <summary>
-        /// btnCancelAndLogout control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button btnCancelAndLogout;
-        
-        /// <summary>
         /// btnVerify control.
         /// </summary>
         /// <remarks>
--- portal/eurona/user/emailVerifycation.aspx.designer.cs
+++ portal/eurona/user/emailVerifycation.aspx.designer.cs
@@ -103,15 +103,6 @@ namespace Eurona.User {
         protected global::System.Web.UI.WebControls.Label lblValidatorTextPwdRepeat;
         
         /// <summary>
-        /// btnCancelAndLogout control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button btnCancelAndLogout;
-        
-        /// <summary>
         /// btnContinueToFinish control.
         /// </summary>
         /// <remarks>
